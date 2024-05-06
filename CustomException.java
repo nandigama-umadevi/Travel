a265eafe7f5ac5b473da89b56533dfd57c56aa70
nandigama-umadevi <85564503+nandigama-umadevi@users.noreply.github.com>
2024-05-06 11:30:39 +0530

    Add files via upload

---
+++ CustomException.java
@@ -0,0 +1,7 @@
+package io.nuggets.chicken_nugget.exceptions;
+
+public class CustomException extends RuntimeException {
+    public CustomException(String message) {
+        super(message);
+    }
+}
